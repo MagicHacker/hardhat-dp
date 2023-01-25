316259e7f5c278c2cdb22beface151e3a4bddd90
MagicHacker <zxmagichacker@gmail.com>
2023-01-25 21:25:00 +0800

    feat: 编写部署

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// 部署
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // 账户
+  const [deployer] = await ethers.getSigners();
+  console.warn("部署合约的账户:", deployer.address);
+  console.warn("账户余额:", (await deployer.getBalance()).toString());
+  // 合约对象
+  const Token = await ethers.getContractFactory("Token");
+  // 不熟
+  const token = await Token.deploy();
+  console.warn("合约地址:", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main().then(() => {
+  process.exit(0);
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
+})
